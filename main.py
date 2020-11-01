84442903fda7b3afb5a8c40efd8bbefa8de5d7c8
oleslav <oleslav.boychuk@gmail.com>
2020-11-01 22:03:33 +0200

    Flask Start

---
+++ main.py
@@ -0,0 +1,4 @@
+from itertools import product
+
+print(list(product(('python 3.8.*', 'python 3.7.*', 'python 3.6.*'),
+                   ('venv + requirements.txt', 'virtualenv + requirements.txt', 'poetry', 'pipenv')))[2 - 1])
