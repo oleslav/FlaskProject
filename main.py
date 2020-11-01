152eca540d5eab74a5c8e50112a10af83bb9260f
oleslav <oleslav.boychuk@gmail.com>
2020-11-01 22:11:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from itertools import product
+from flask import Flask
 
-print(list(product(('python 3.8.*', 'python 3.7.*', 'python 3.6.*'),
-                   ('venv + requirements.txt', 'virtualenv + requirements.txt', 'poetry', 'pipenv')))[2 - 1])
+app = Flask(__name__)
 
+
+@app.route('/api/v1/hello-world-2')
+def hello_world():
+    return 'Hello World 2'
+
+
+if __name__ == '__main__':
+    app.run()
