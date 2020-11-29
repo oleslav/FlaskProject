75b4dce76e36cafbc3d3519159a267feda3a82b9
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2020-11-29 18:24:24 +0200

    Lab orm

--- app.py
+++ app.py
@@ -1,20 +1,58 @@
-from wsgiref.simple_server import make_server
-from flask import Flask
-
+from flask import Flask, jsonify, request
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(
+    user='root', password='root', server='localhost', database='pp_orm')
+db = SQLAlchemy(app)
+
+engine = db.engine
+Base = db.Model
+
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
 
 
+# Provide a method to create access tokens. The create_access_token()
+# function is used to actually generate the token, and you can return
+# it to the caller however you choose.
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    if username == '' or password == '':
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token), 200
+
+
+# Protect a view with jwt_required, which requires a valid access token
+# in the request to access.
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 @app.route('/')
-def start():
-    return 'Hello World'
-
-
-@app.route('/api/v1/hello-world-2')
 def hello_world():
-    return 'Hello World 2'
+    print(get_jwt_identity())
+    return 'Hello World!'
 
 
-with make_server('', 8080, app) as server:
-    print("Something http://127.0.0.1:8080")
-    print("Something http://127.0.0.1:8080/api/v1/hello-world-2")
-    server.serve_forever()
+# if __name__ == '__main__':
+#     app.run()
